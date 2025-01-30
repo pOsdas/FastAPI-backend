865ac8295a68f3fc98f82c14488352597a92b3dc
pOsdas <ratmir1099@mail.ru>
2025-01-30 17:05:49 +0300

    add schema for user

---
+++ user_service/users/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr, ConfigDict
+from typing import Annotated
+from annotated_types import MinLen, MaxLen
+from datetime import datetime
+
+
+class CreateUser(BaseModel):
+    username: Annotated[str, MinLen(3), MaxLen(32)]
+    email: EmailStr
+
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(strict=True, from_attributes=True)
+
+    id: int
+    username: str
+    email: EmailStr | None = None
+    is_active: bool = True
+    created_at: datetime
+    updated_at: datetime
