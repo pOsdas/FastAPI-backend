e5423c5210e6b57dc8f7b3e511b9298967df8fc4
pOsdas <ratmir1099@mail.ru>
2025-01-31 17:33:23 +0300

    ~

---
+++ user_service/core/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    "Base",
+    'db_helper',
+    "DatabaseHelper",
+    "User",
+)
+
+from .base import Base
+from .db_helper import db_helper, DatabaseHelper
+from .user import User
