f96f93e7e6d56b8bd41f53d5a1f44fe977bdeb29
pOsdas <ratmir1099@mail.ru>
2025-02-04 00:41:47 +0300

    fix the imports

--- user_service/core/models/base.py
+++ user_service/core/models/base.py
@@ -7,19 +7,19 @@ from sqlalchemy.orm import (
 )
 
 from utils import camel_case_to_snake_case
-from ..config import settings
+from user_service.core.config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return camel_case_to_snake_case(cls.__name__)
-
     metadata = MetaData(
         naming_convention=settings.db.naming_conventions,
     )
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return camel_case_to_snake_case(cls.__name__)
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
