1faaf61c354c723fbb5feb957d416f262c5024ea
pOsdas <ratmir1099@mail.ru>
2025-02-07 01:15:04 +0300

    add base models to core

---
+++ auth_service/core/models/__init__.py
@@ -0,0 +1,8 @@
+__all__ = (
+    "Base",
+    'db_helper',
+    "DatabaseHelper",
+)
+
+from .base import Base
+from .db_helper import db_helper, DatabaseHelper
---
+++ auth_service/core/models/base.py
@@ -0,0 +1,25 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    declared_attr,
+)
+
+from utils import camel_case_to_snake_case
+from auth_service.core.config import settings
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_conventions,
+    )
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return camel_case_to_snake_case(cls.__name__)
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
