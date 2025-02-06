c244b28f36e2204b20ea08c8bf7590a862b716df
pOsdas <ratmir1099@mail.ru>
2025-02-07 01:15:41 +0300

    add helpers to work with jwt

---
+++ auth_service/api/api_v1/utils/helpers.py
@@ -0,0 +1,52 @@
+from datetime import timedelta
+
+from auth_service.core import security as auth_utils
+from auth_service.core.config import settings
+from auth_service.core.schemas import AuthUser
+
+
+TOKEN_TYPE_FIELD = "type"
+ACCESS_TOKEN_TYPE = "access"
+REFRESH_TOKEN_TYPE = "refresh"
+
+
+def create_jwt(
+        token_type: str,
+        token_data: dict,
+        expires_in: int = settings.auth_jwt.access_token_expires_in,
+        expire_timedelta: timedelta | None = None,
+) -> str:
+    jwt_payload = {TOKEN_TYPE_FIELD: token_type}
+    jwt_payload.update(token_data)
+    return auth_utils.encode_jwt(
+        payload=jwt_payload,
+        expires_in=expires_in,
+        expire_timedelta=expire_timedelta,
+    )
+
+
+def create_access_token(user: AuthUser) -> str:
+    jwt_payload = {
+        # subject
+        "sub": user.username,
+        "username": user.username,
+        "email": user.email,
+    }
+
+    return create_jwt(
+        token_type=ACCESS_TOKEN_TYPE,
+        token_data=jwt_payload,
+        expires_in=settings.auth_jwt.access_token_expires_in,
+    )
+
+
+def create_refresh_token(user: AuthUser) -> str:
+    jwt_payload = {
+        "sub": user.username,
+        "username": user.username,
+    }
+    return create_jwt(
+        token_type=REFRESH_TOKEN_TYPE,
+        token_data=jwt_payload,
+        expire_timedelta=timedelta(days=settings.auth_jwt.refresh_token_expire_days),
+    )
