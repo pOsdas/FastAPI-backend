8d06dd24b9ac45409116a14c33bc060cb817203f
pOsdas <ratmir1099@mail.ru>
2025-02-06 23:51:02 +0300

    remove "/hello/" router + change users_router path

--- user_service/main.py
+++ user_service/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from core.config import settings
-from users.views import router as users_router
+from user_service.api.api_v1.users import router as users_router
 from core.models.db_helper import db_helper
 
 
@@ -26,12 +26,6 @@ def hello_index():
     return {
         "message": "Hello from User-Service"
     }
-
-
-@main_app.get("/hello/")
-def hello(name: str = "World"):
-    name = name.strip().title()
-    return {"message": f"Hello, {name}!"}
 
 
 if __name__ == "__main__":
