37485920973b76756b20a3c7f5c15079e09204f4
pOsdas <ratmir1099@mail.ru>
2025-02-07 01:14:38 +0300

    created security.py containing functions for working with tokens and passwords

---
+++ auth_service/core/security.py
@@ -0,0 +1,56 @@
+import jwt
+import bcrypt
+from datetime import datetime, timezone, timedelta
+
+from .config import settings
+
+
+def encode_jwt(
+        payload: dict,
+        private_key: str = settings.auth_jwt.private_key_path.read_text(),
+        algorithm: str = settings.auth_jwt.algorithm,
+        expires_in: int = settings.auth_jwt.access_token_expires_in,  # minutes
+        expire_timedelta: timedelta | None = None,
+):
+    to_encode = payload.copy()
+    now = datetime.now(timezone.utc)
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expires_in)
+    to_encode.update(
+        exp=expire,
+        iat=now,
+    )
+    encoded = (
+        jwt.encode(
+            to_encode,
+            private_key,
+            algorithm=algorithm)
+    )
+    return encoded
+
+
+def decode_jwt(
+        token: str | bytes,
+        public_key: str = settings.auth_jwt.public_key_path.read_text(),
+        algorithm: str = settings.auth_jwt.algorithm,
+):
+    decoded = jwt.decode(token, public_key, algorithms=[algorithm])
+    return decoded
+
+
+def hash_password(password: str) -> bytes:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    return bcrypt.hashpw(pwd_bytes, salt)
+
+
+def validate_password(
+        password: str,
+        hashed_password: bytes
+) -> bool:
+    return bcrypt.checkpw(
+        password.encode(),
+        hashed_password=hashed_password,
+    )
