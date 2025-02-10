90fbf378b18fa3d0128cd26575f1946534198110
pOsdas <ratmir1099@mail.ru>
2025-02-11 01:26:58 +0300

    fix import (utils -> security)

--- auth_service/crud/crud.py
+++ auth_service/crud/crud.py
@@ -1,16 +1,16 @@
-from auth_service.api.api_v1.utils import helpers
+from auth_service.core.security import hash_password
 from auth_service.core.schemas import AuthUser
 
 # fake users db
 
 john = AuthUser(
     username="john",
-    password=helpers.hash_password("qwerty"),
+    password=hash_password("qwerty"),
     email="john@example.com"
 )
 sam = AuthUser(
     username="sam",
-    password=helpers.hash_password("secret"),
+    password=hash_password("secret"),
 )
 users_db: dict[str, AuthUser] = {
     john.username: john,
