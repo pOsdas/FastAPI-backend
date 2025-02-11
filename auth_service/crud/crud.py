719d1049a961a1128bc8e4064208f31a12d9bcff
pOsdas <ratmir1099@mail.ru>
2025-02-11 19:24:42 +0300

    add usernames_to_password + static_auth_token_to_username to crud

--- auth_service/crud/crud.py
+++ auth_service/crud/crud.py
@@ -1,8 +1,7 @@
 from auth_service.core.security import hash_password
 from auth_service.core.schemas import AuthUser
 
-# fake users db
-
+# ### fake users db
 john = AuthUser(
     username="john",
     password=hash_password("qwerty"),
@@ -16,3 +15,9 @@ users_db: dict[str, AuthUser] = {
     john.username: john,
     sam.username: sam,
 }
+
+usernames_to_password = {"demo_user": "demo_password"}
+static_auth_token_to_username = {
+    "90609ed991fcca984411d4b6e1ba7": "demo_user",
+}
+# ### never do like that
