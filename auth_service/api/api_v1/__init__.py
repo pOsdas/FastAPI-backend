0a3d9bf31340cd95524bd2e14cea8d573b82a461
pOsdas <ratmir1099@mail.ru>
2025-02-11 19:26:37 +0300

    add jwt router

--- auth_service/api/api_v1/__init__.py
+++ auth_service/api/api_v1/__init__.py
@@ -3,11 +3,23 @@ from fastapi import APIRouter
 from auth_service.core.config import settings
 
 from .auth import router as auth_router
+from .jwt_auth import router as jwt_router
 
-router = APIRouter(
+api_v1_router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
-router.include_router(
+
+api_v1_router.include_router(
     auth_router,
     prefix=settings.api.v1.auth,
 )
+
+api_v1_router.include_router(
+    jwt_router,
+    prefix=settings.api.v1.jwt,
+)
+
+
+
+
+
