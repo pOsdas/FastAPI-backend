987b8755a1ea5629287d9c498008fd81b1fac064
pOsdas <ratmir1099@mail.ru>
2025-02-11 19:27:19 +0300

    fix imports

--- auth_service/api/__init__.py
+++ auth_service/api/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
 from auth_service.core.config import settings
-from .api_v1 import router as router_api_v1
+from .api_v1 import api_v1_router
 
 router = APIRouter(
     prefix=settings.api.prefix,
 )
-router.include_router(router_api_v1)
+router.include_router(api_v1_router)
