945c04da4c47c93fcf1e140765206f1ff77d4774
pOsdas <ratmir1099@mail.ru>
2025-02-18 01:09:41 +0300

    add "hashed_password" + "refresh_token" var to AuthUser model

--- auth_service/core/schemas/auth_user.py
+++ auth_service/core/schemas/auth_user.py
@@ -3,5 +3,7 @@ from pydantic import BaseModel, EmailStr
 
 class AuthUser(BaseModel):
     username: str
+    hashed_password: bytes
+    refresh_token: str
     email: EmailStr | None = None
     active: bool = True
