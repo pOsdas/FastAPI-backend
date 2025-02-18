be2c2ebeacf7704601b87fe2f35b6342546dbaab
pOsdas <ratmir1099@mail.ru>
2025-02-19 01:27:08 +0300

    add endpoint paths

--- user_service/api/api_v1/users.py
+++ user_service/api/api_v1/users.py
@@ -1,7 +1,9 @@
 from typing import Annotated
+from datetime import datetime
 
 from fastapi import (
     APIRouter,
+    HTTPException,
     Depends,
     BackgroundTasks,
 )
@@ -9,26 +11,28 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from user_service.api.api_v1.utils.send_welcome_email import send_welcome_email
 from user_service.core.models import db_helper
-from user_service.core.schemas.user import CreateUser, ReadUser
+from user_service.core.schemas.user import CreateUser, ReadUser, UserSchema
 from user_service.crud import crud
 
 router = APIRouter(
-    tags=["Users"],
+    prefix="/users", tags=["Users"],
 )
 
 
-@router.get("", response_model=list[ReadUser])
-async def get_users(
-        session: Annotated[
-            AsyncSession,
-            Depends(db_helper.session_getter),
-        ],
-):
-    users = await crud.get_all_users(session=session)
-    return users
+fake_users_db = {
+    1: {
+        "id": 1,
+        "username": "john_doe",
+        "email": "john@example.com",
+        "is_active": True,
+        "created_at": datetime.now(),
+        "updated_at": datetime.now(),
+    },
+    # Можно добавить и других пользователей
+}
 
 
-@router.post("", response_model=ReadUser)
+@router.post("/create_user", response_model=ReadUser)
 async def create_user(
         session: Annotated[
             AsyncSession,
@@ -44,4 +48,26 @@ async def create_user(
     )
     background_tasks.add_task(send_welcome_email, user_id=user.id)
     return user
+
+
+@router.get("/get_users", response_model=list[ReadUser])
+async def get_users(
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter),
+        ],
+):
+    users = await crud.get_all_users(session=session)
+    return users
+
+
+@router.get("/{user_id}", response_model=UserSchema)
+async def get_user(user_id: int):
+    user = fake_users_db.get(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+
 
