72202ffb36253a32bfd9ea5c253d2fd6a78bae2d
pOsdas <ratmir1099@mail.ru>
2025-02-23 02:56:05 +0300

    add oauth2 router

--- auth_service/api/api_v1/__init__.py
+++ auth_service/api/api_v1/__init__.py
@@ -4,6 +4,7 @@ from auth_service.core.config import settings
 
 from .auth import router as auth_router
 from .jwt_auth import router as jwt_router
+from .oauth import oauth_router
 
 api_v1_router = APIRouter(
     prefix=settings.api.v1.prefix,
@@ -17,6 +18,11 @@ api_v1_router.include_router(
 api_v1_router.include_router(
     jwt_router,
     # prefix=settings.api.v1.jwt,
+)
+
+api_v1_router.include_router(
+    oauth_router,
+    # prefix=settings.api.v1.oauth,
 )
 
 
