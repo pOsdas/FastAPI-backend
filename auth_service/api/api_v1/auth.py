cb3a57a66d7df5c52c12d7625107e9bb283ca8ac
pOsdas <ratmir1099@mail.ru>
2025-02-25 00:35:31 +0300

    moved functions for testing cookies into a separate file

--- auth_service/api/api_v1/auth.py
+++ auth_service/api/api_v1/auth.py
@@ -115,24 +115,6 @@ def check_token_auth(
     }
 
 
-@router.get("/set_session")
-async def set_session(request: Request):
-    """
-    Тестирование cookies
-    """
-    request.session["test_key"] = "test_value"
-    return {"message": "Session set"}
-
-
-@router.get("/get_session")
-async def get_session(request: Request):
-    """
-    Тестирование cookies
-    """
-    value = request.session.get("test_key", "not found")
-    return {"session_value": value}
-
-
 @router.post("/login-cookie/")
 def demo_auth_login_cookie(
         response: Response,
