26fe785b41ba07957a7373bfbab8e62c12362c50
pOsdas <ratmir1099@mail.ru>
2025-02-26 00:14:54 +0300

    fix error with db "connection was closed in the middle of operation". Now the database will be created if it was not there

--- user_service/main.py
+++ user_service/main.py
@@ -1,8 +1,12 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
 import uvicorn
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from core.config import settings
+from core.models import Base
 from user_service.api import router as users_router
 from core.models.db_helper import db_helper
 
@@ -10,6 +14,13 @@ from core.models.db_helper import db_helper
 @asynccontextmanager
 async def lifespan(users_app: FastAPI):
     # startup
+    loop = asyncio.get_event_loop()
+    with ThreadPoolExecutor() as executor:
+        await loop.run_in_executor(executor, db_helper.create_db_if_not_exists)
+
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
     yield
     # shutdown
     await db_helper.dispose()
