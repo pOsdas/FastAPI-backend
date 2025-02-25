26142614a942104f59c37ee8c953ca426b826c2c
pOsdas <ratmir1099@mail.ru>
2025-02-26 00:18:16 +0300

    fix type of "is_active" column + fix updated_at column behavior

--- user_service/core/models/user.py
+++ user_service/core/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, TIMESTAMP, func
+from sqlalchemy import String, TIMESTAMP, func, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -7,10 +7,10 @@ from .base import Base
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True, index=True)
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
-    is_active: Mapped[str] = mapped_column(default=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
 
     created_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, server_default=func.now())
-    updated_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, server_default=func.now(), onupdate=func.now)
+    updated_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}(id={self.id}, username={self.username!r})"
