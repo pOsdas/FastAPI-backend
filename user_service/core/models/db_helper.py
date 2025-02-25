1f31ee13b1884fb6ab2cd0d48702bee450b7f98a
pOsdas <ratmir1099@mail.ru>
2025-02-26 00:19:22 +0300

    add create_db_if_not_exists to create database at the first launch

--- user_service/core/models/db_helper.py
+++ user_service/core/models/db_helper.py
@@ -47,8 +47,34 @@ class DatabaseHelper:
             except Exception as e:
                 print(f"Ошибка в сессии: {e}")
                 raise
-            finally:
-                print("Сессия закрывается")
+
+    @staticmethod
+    def create_db_if_not_exists():
+        import psycopg2
+        from urllib.parse import urlparse
+
+        parsed = urlparse(str(settings.db.url))
+        target_db = parsed.path.lstrip("/")
+        user = parsed.username
+        password = parsed.password
+        host = parsed.hostname
+        port = parsed.port
+
+        conn = psycopg2.connect(dbname="postgres", user=user, password=password, host=host, port=port)
+        conn.autocommit = True
+        cur = conn.cursor()
+
+        cur.execute("SELECT 1 FROM pg_database WHERE datname = %s", (target_db,))
+        exists = cur.fetchone()
+
+        if not exists:
+            print(f"База данных '{target_db}' не найдена. Создаю...")
+            cur.execute(f"CREATE DATABASE {target_db}")
+        else:
+            print(f"База данных '{target_db}' уже существует.")
+
+        cur.close()
+        conn.close()
 
 
 db_helper = DatabaseHelper(
