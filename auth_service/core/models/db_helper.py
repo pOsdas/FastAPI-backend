6b5e867516c837427fd6bd9ad6316c442c9fa93e
pOsdas <ratmir1099@mail.ru>
2025-02-26 23:47:19 +0300

    fix error with db "connection was closed in the middle of operation". Now the database will be created if it was not there

--- auth_service/core/models/db_helper.py
+++ auth_service/core/models/db_helper.py
@@ -42,6 +42,34 @@ class DatabaseHelper:
         async with self.session_factory() as session:
             yield session
 
+    @staticmethod
+    def create_db_if_not_exists():
+        import psycopg2
+        from urllib.parse import urlparse
+
+        parsed = urlparse(str(settings.db.url))
+        target_db = parsed.path.lstrip("/")
+        user = parsed.username
+        password = parsed.password
+        host = parsed.hostname
+        port = parsed.port
+
+        conn = psycopg2.connect(dbname="postgres", user=user, password=password, host=host, port=port)
+        conn.autocommit = True
+        cur = conn.cursor()
+
+        cur.execute("SELECT 1 FROM pg_database WHERE datname = %s", (target_db,))
+        exists = cur.fetchone()
+
+        if not exists:
+            print(f"База данных '{target_db}' не найдена. Создаю...")
+            cur.execute(f"CREATE DATABASE {target_db}")
+        else:
+            print(f"База данных '{target_db}' уже существует.")
+
+        cur.close()
+        conn.close()
+
 
 db_helper = DatabaseHelper(
     url=str(settings.db.url),
