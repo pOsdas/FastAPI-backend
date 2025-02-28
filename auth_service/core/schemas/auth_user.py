d7a71aaedd787ba61be2d1d77ee3f22e67efc9e4
pOsdas <ratmir1099@mail.ru>
2025-02-28 23:34:04 +0300

    add updated_at field to AuthUser schema

--- auth_service/core/schemas/auth_user.py
+++ auth_service/core/schemas/auth_user.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 
@@ -6,6 +8,7 @@ class AuthUser(BaseModel):
     user_id: int
     username: str
     password: bytes
-    email: EmailStr | None = None
+    email: Optional[EmailStr] = None
     is_active: bool = True
-    refresh_token: Optional[str]
+    refresh_token: Optional[str] = None
+    updated_at: Optional[datetime] = None
