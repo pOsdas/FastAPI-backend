08f657dbd81b8afe88c4db89d95808571024610e
pOsdas <ratmir1099@mail.ru>
2025-03-02 01:24:47 +0300

    add AuthUser model to __init__

--- auth_service/core/models/__init__.py
+++ auth_service/core/models/__init__.py
@@ -2,7 +2,9 @@ __all__ = (
     "Base",
     'db_helper',
     "DatabaseHelper",
+    "AuthUser"
 )
 
 from .base import Base
 from .db_helper import db_helper, DatabaseHelper
+from .auth_user import AuthUser
