1e61e64b1e2936a9bf6914e157c8c71e776f4659
pOsdas <ratmir1099@mail.ru>
2025-03-02 19:32:11 +0300

    add get_user and delete_user to crud

--- auth_service/crud/crud.py
+++ auth_service/crud/crud.py
@@ -53,3 +53,30 @@ async def get_all_users(
     stmt = select(AuthUserModel).order_by(AuthUserModel.user_id)
     result = await session.scalars(stmt)
     return result.all()
+
+
+async def get_auth_user(
+        user_id: int,
+        session: AsyncSession
+):
+    stmt = select(AuthUserModel).where(AuthUserModel.user_id == user_id)
+    result = await session.execute(stmt)
+    auth_user = result.scalar_one_or_none()
+    return auth_user
+
+
+async def delete_auth_user(
+        user_id: int,
+        session: AsyncSession
+):
+    stmt = select(AuthUserModel).where(AuthUserModel.user_id == user_id)
+    result = await session.execute(stmt)
+    auth_user = result.scalar_one_or_none()
+
+    await session.delete(auth_user)
+    try:
+        await session.commit()
+    except Exception as e:
+        await session.rollback()
+        print(f"Failed to delete user: {str(e)}")
+
