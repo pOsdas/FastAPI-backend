fecdee76a8a7749d798c9c53e1cb1184541bcf6a
pOsdas <ratmir1099@mail.ru>
2025-05-08 01:31:38 +0300

    removed fake db

--- auth_service/crud/users_crud.py
+++ auth_service/crud/users_crud.py
@@ -5,7 +5,6 @@ update
 delete
 """
 from typing import Sequence
-
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -13,39 +12,6 @@ from auth_service.core.models import AuthUser as AuthUserModel
 from auth_service.core.schemas import AuthUser as AuthUserSchema
 from auth_service.core.security import hash_password
 # from auth_service.core.schemas import AuthUser
-
-# ### fake users db
-john = AuthUserModel(
-    user_id=1,
-    password=hash_password("qwerty"),
-    # updated_at
-    refresh_token="dummy_refresh_token"
-)
-
-sam = AuthUserModel(
-    user_id=2,
-    password=hash_password("secret"),
-    # updated_at
-    refresh_token="second_dummy_refresh_token"
-)
-
-
-test = AuthUserModel(
-    user_id=3,
-    password=hash_password("test"),
-    # updated_at
-    refresh_token="second_dummy_refresh_token"
-)
-
-users_db: dict[int, AuthUserModel] = {
-    john.user_id: john,
-    sam.user_id: sam,
-}
-user_id_to_password = {"3": "test"}
-static_auth_token_to_user_id = {
-    "90609ed991fcca984411d4b6e1ba7": john.user_id,
-}
-# ### never do like that
 
 
 async def get_all_users(
@@ -80,14 +46,4 @@ async def delete_auth_user(
     except Exception as e:
         await session.rollback()
         print(f"Failed to delete user: {str(e)}")
-
-
-async def update_refresh_token(
-    session: AsyncSession,
-    user: AuthUserModel,
-    new_token: str
-):
-    user.refresh_token = new_token
-    session.add(user)
-    await session.commit()
 
