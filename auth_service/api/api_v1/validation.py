997ac68b6c27c64e0437f3e6966f630d3c77a1d6
pOsdas <ratmir1099@mail.ru>
2025-05-10 17:46:54 +0300

    Fixed get_user_by_token_sub return type

--- auth_service/api/api_v1/validation.py
+++ auth_service/api/api_v1/validation.py
@@ -6,10 +6,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 from sqlalchemy.future import select
 
+from auth_service.crud.users_crud import get_user_service_user_by_id
 from auth_service.core.models import db_helper
 from .utils.helpers import TOKEN_TYPE_FIELD, ACCESS_TOKEN_TYPE, REFRESH_TOKEN_TYPE
 from auth_service.core import security
 from auth_service.core.models import AuthUser as AuthUserModel
+from auth_service.core.schemas import CombinedUserSchema
 
 oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl="api/v1/auth/jwt/login/",
@@ -37,7 +39,15 @@ def validate_token_type(
         payload: dict,
         token_type: str
 ) -> bool:
+    allowed_types = {ACCESS_TOKEN_TYPE, REFRESH_TOKEN_TYPE}
     current_token_type = payload.get(TOKEN_TYPE_FIELD)
+
+    if current_token_type not in allowed_types:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Invalid token type: {current_token_type}",
+        )
+
     if current_token_type == token_type:
         return True
     raise HTTPException(
@@ -52,18 +62,30 @@ async def get_user_by_token_sub(
                 AsyncSession,
                 Depends(db_helper.session_getter)
             ]
-) -> AuthUserModel:
-    user_id: int | None = payload.get("sub")
+) -> CombinedUserSchema:
+    user_id: int | None = int(payload.get("sub"))
     stmt = select(AuthUserModel).where(AuthUserModel.user_id == user_id)
     result = await session.execute(stmt)
     user = result.scalar_one_or_none()
 
-    if user:
-        return user
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token invalid or expired"
+        )
 
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Token invalid or expired"
+    # Запрос к user_service
+    try:
+        response = await get_user_service_user_by_id(user_id=user_id)
+    except HTTPException as exc:
+        raise exc
+
+    user_profile = response.json()
+    email = user_profile.get("email")
+
+    return CombinedUserSchema(
+        user_id=user.user_id,
+        email=email
     )
 
 
